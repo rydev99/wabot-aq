99f277bdb6d9ee1f62d687993ac64084c1b40261
rydev99 <100022705+rydev99@users.noreply.github.com>
2022-03-29 21:33:21 +0700

    update case.js

--- plugins/case.js
+++ plugins/case.js
@@ -14,6 +14,8 @@ function handler(m, { text }) {
         }
     }))
 }
+handler.help = ['alay']
+handler.tags = ['tools']
 handler.command = ['alay', 'case']
 
-module.exports = handler
+export defaulthandler
